ee7665a562ad7780d9b0f88ebeaffc24c24dc71d
Kri <kritika.murugan@informatik.hs-fulda.de>
2021-11-13 10:43:00 +0100

    initial commit

---
+++ app/src/main/java/com/example/mytracker/MapsActivity.java
@@ -0,0 +1,165 @@
+package com.example.mytracker;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.mytracker.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    MyDbAdapter helper;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int locationRQ = 101;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            checkPermissions(Manifest.permission.ACCESS_FINE_LOCATION, "Location", locationRQ);
+            checkPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, "Location", locationRQ);
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        helper = new MyDbAdapter(this);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        ArrayList<ArrayList<Object>> data = helper.getData();
+//        Toast.makeText(this, data.toString(), Toast.LENGTH_LONG).show();
+
+        for (ArrayList<Object> object : data) {
+            Toast.makeText(getApplicationContext(), "Object:" + object, Toast.LENGTH_SHORT).show();
+                double lat = (double) object.get(0);
+                double longi = (double) object.get(1);
+                LatLng latLng = new LatLng(lat,longi);
+                mMap.addMarker(new MarkerOptions().position(latLng));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 9.0F));
+
+
+        }
+
+    }
+
+    public void viewData(View view) {
+//        ArrayList<Pair<Double, Double>> data = helper.getData();
+//        Toast.makeText(this, data.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void addMarker(View view) {
+
+
+        Date currentTime = Calendar.getInstance().getTime();
+
+        Toast.makeText(getApplicationContext(), "Current Time: " + currentTime, Toast.LENGTH_SHORT).show();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Permission not granted", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            double lat = location.getLatitude();
+                            double longi = location.getLongitude();
+                            LatLng latLng = new LatLng(lat,longi);
+                            mMap.addMarker(new MarkerOptions().position(latLng));
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 9.0F));
+                            helper.insertData("kri", lat, longi, currentTime.toString());
+                        }
+                    }
+                });
+
+        Toast.makeText(getApplicationContext(), "Added!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void checkPermissions(String permission, String name, int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), permission) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), name + " Permission Granted", Toast.LENGTH_SHORT).show();
+                shouldShowRequestPermissionRationale(permission);
+                showDialog(permission, name, requestCode);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Permissions refused " + requestCode, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Permissions granted " + requestCode, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showDialog(String permission, String name, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        new AlertDialog.Builder(this)
+                .setTitle("Grant Location Permission")
+                .setMessage("This will let you save your location using your GPS")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{permission}, requestCode);
+                    }
+                })
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
---
+++ app/src/main/java/com/example/mytracker/MyDbAdapter.java
@@ -0,0 +1,124 @@
+package com.example.mytracker;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Pair;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MyDbAdapter {
+    MyDbHelper myhelper;
+
+    public MyDbAdapter(Context context) {
+        myhelper = new MyDbHelper(context);
+    }
+
+    public long insertData(String title, double lat, double longi, String time) {
+        SQLiteDatabase dbb = myhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+//        contentValues.put(MyDbHelper.TITLE, title);
+        contentValues.put(MyDbHelper.LATITUDE, lat);
+        contentValues.put(MyDbHelper.LONGITUDE, longi);
+//        contentValues.put(MyDbHelper.TIME, time);
+        long id = dbb.insert(MyDbHelper.TABLE_NAME, null, contentValues);
+        return id;
+    }
+
+    public ArrayList<ArrayList<Object>> getData() {
+        insertHistoricData();
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        String[] columns = {MyDbHelper.UID, MyDbHelper.LATITUDE, MyDbHelper.LONGITUDE};
+        Cursor cursor = db.query(MyDbHelper.TABLE_NAME, columns, null, null, null, null, null);
+
+        ArrayList<ArrayList<Object>> locationArray = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            try {
+                ArrayList<Object> tempList = new ArrayList<Object>();
+//                int cid = 1;
+//                tempList.add(cursor.getString(cursor.getColumnIndexOrThrow(MyDbHelper.TITLE)));
+                tempList.add(cursor.getDouble(cursor.getColumnIndexOrThrow(MyDbHelper.LATITUDE)));
+                tempList.add(cursor.getDouble(cursor.getColumnIndexOrThrow(MyDbHelper.LONGITUDE)));
+//                tempList.add(cursor.getString(cursor.getColumnIndexOrThrow(MyDbHelper.TIME)));
+                locationArray.add(tempList);
+
+            } catch (Exception e) {
+                Toast.makeText(myhelper.context, "Error in fetching values", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return locationArray;
+    }
+
+    static class MyDbHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "mapDB";    // Database Name
+        private static final String TABLE_NAME = "history";   // Table Name
+        private static final int DATABASE_Version = 1;    // Database Version
+        private static final String UID = "_id";     // Column 1 (Primary Key)
+        private static final String TITLE = "Title"; // Column 2
+        private static final String LATITUDE = "Latitude";    //Column 3
+        private static final String LONGITUDE = "Longitude";    // Column 4
+        private static final String TIME = "Time";    // Column 5
+        private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
+                " (" + UID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + LATITUDE + " DECIMAL(3,2) ," + LONGITUDE + " DECIMAL(3,2);";
+        private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        private Context context;
+
+        public MyDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_Version);
+            this.context = context;
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+
+            try {
+                db.execSQL(CREATE_TABLE);
+                Toast.makeText(context, "Table Created", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            try {
+                Toast.makeText(context, "On Upgrade", Toast.LENGTH_SHORT).show();
+                db.execSQL(DROP_TABLE);
+                onCreate(db);
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    //    public int delete(String uname) {
+//        SQLiteDatabase db = myhelper.getWritableDatabase();
+//        String[] whereArgs = {uname};
+//
+//        int count = db.delete(MyDbHelper.TABLE_NAME, MyDbHelper.NAME + " = ?", whereArgs);
+//        return count;
+//    }
+//
+//    public int updateName(String oldName, String newName) {
+//        SQLiteDatabase db = myhelper.getWritableDatabase();
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put(MyDbHelper.NAME, newName);
+//        String[] whereArgs = {oldName};
+//        int count = db.update(MyDbHelper.TABLE_NAME, contentValues, MyDbHelper.NAME + " = ?", whereArgs);
+//        return count;
+//    }
+    public void insertHistoricData() {
+        SQLiteDatabase dbb = myhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        for (int i = 0; i < 3; i++) {
+            contentValues.put(MyDbHelper.LATITUDE, 50.55 + i);
+            contentValues.put(MyDbHelper.LONGITUDE, 9.68 + i);
+            long id = dbb.insert(MyDbHelper.TABLE_NAME, null, contentValues);
+        }
+    }
+
+}
